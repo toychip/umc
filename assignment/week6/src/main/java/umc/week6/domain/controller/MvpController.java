84d84c5b8f1992a84f49e0950f370180f3057fcd
toychip <dlawnsgud427@naver.com>
2023-11-10 13:25:01 +0900

    feat: week6 리뷰 작성 api 추가

--- assignment/week6/src/main/java/umc/week6/domain/controller/MvpController.java
+++ assignment/week6/src/main/java/umc/week6/domain/controller/MvpController.java
@@ -4,9 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import umc.week6.domain.dto.request.ReviewCreateRequest;
 import umc.week6.domain.dto.response.HomeResponse;
 import umc.week6.domain.service.HomeService;
+import umc.week6.domain.service.ReviewService;
 
 
 @RestController
@@ -17,11 +21,18 @@ public class MvpController {
 
     // 모든 기능은 로그인 후에 접근 할 수 있도록 가정
     private final HomeService homeService;
+    private final ReviewService reviewService;
 
     // 홈 화면 스크롤
     @GetMapping("/home")
     public Page<HomeResponse> getHome(Pageable pageable) {
         return homeService.getHome(pageable);
+    }
+
+    // 리뷰 작성
+    @PostMapping("/review")
+    public void writeReview(@RequestBody ReviewCreateRequest request) {
+        reviewService.writeReview(request);
     }
 
 
---
+++ assignment/week6/src/main/java/umc/week6/domain/service/ReviewService.java
@@ -0,0 +1,15 @@
+package umc.week6.domain.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import umc.week6.domain.dto.request.ReviewCreateRequest;
+import umc.week6.domain.dto.response.ReviewDetailResponse;
+import umc.week6.domain.dto.response.ReviewListResponse;
+
+@Service
+public class ReviewService {
+    public void writeReview(ReviewCreateRequest request) {
+    }
+
+}
